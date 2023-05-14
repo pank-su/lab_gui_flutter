11b9919a985356e0e7c07b87df5b31d1c57d801f
Вася Панков <frostkslo1@yandex.ru>
2023-05-14 21:39:19 +0300

    Теперь можно просматривать древовидную структуру

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fancy_tree_view/flutter_fancy_tree_view.dart';
+import 'package:lab_gui_flutter/models/base_model.dart';
+import 'package:lab_gui_flutter/repository.dart';
 import 'package:lab_gui_flutter/screens/auth.dart';
 import 'package:lab_gui_flutter/screens/collection_page.dart';
 import 'package:provider/provider.dart';
@@ -103,12 +106,12 @@ class _MainPageState extends State<MainPage> {
                           showDialog(
                               context: context,
                               builder: (context) {
-                                return Dialog(
+                                return const Dialog(
                                   child: Text("data"),
                                 );
                               });
                         },
-                        child: Icon(Icons.add),
+                        child: const Icon(Icons.add),
                       ),
                       selectedIndex: selectedIndex,
                       backgroundColor: surfaceContainer,
@@ -141,12 +144,101 @@ class _MainPageState extends State<MainPage> {
   }
 }
 
-class TopologyPage extends StatelessWidget {
+class TopologyPage extends StatefulWidget {
   const TopologyPage({super.key});
 
   @override
+  State<TopologyPage> createState() => _TopologyPageState();
+}
+
+class _TopologyPageState extends State<TopologyPage> {
+  final topology = {
+    BaseModelsTypes.order: getFamiliesById,
+    BaseModelsTypes.family: getGenusesById,
+    BaseModelsTypes.genus: getKindsById,
+  };
+  late final TreeController<BaseModel> treeController;
+
+  Map<BaseModel, List<BaseModel>> childrenMap = {};
+  BaseModel father =
+      BaseModel(id: 0, name: "father", type: BaseModelsTypes.father);
+
+  var loadingModels = <BaseModel>[];
+
+  Future<void> loadOrders() async {
+    var orders = await getOrders();
+    setState(() {
+      childrenMap[father] = orders;
+      treeController.roots = childrenProvider(father);
+    });
+  }
+
+  @override
+  void initState() {
+    childrenMap[father] = [];
+
+    treeController = TreeController(
+        roots: childrenProvider(father), childrenProvider: childrenProvider);
+
+    loadOrders();
+
+    super.initState();
+  }
+
+  Iterable<BaseModel> childrenProvider(BaseModel baseModel) {
+    return childrenMap[baseModel] ?? const Iterable.empty();
+  }
+
+  Widget getLeading(BaseModel baseModel) {
+    if (loadingModels.contains(baseModel)) {
+      return CircularProgressIndicator();
+    }
+
+    late final VoidCallback? onPressed;
+    late final bool? isOpen;
+
+    final List<BaseModel>? children = childrenMap[baseModel];
+
+    if (baseModel.name != null &&
+        baseModel.type != BaseModelsTypes.kind &&
+        children == null) {
+      isOpen = false;
+      onPressed = () async {
+        setState(() {
+          loadingModels.add(baseModel);
+        });
+        final list = await topology[baseModel.type]!(baseModel);
+        childrenMap[baseModel] = list;
+        loadingModels.remove(baseModel);
+        treeController.expand(baseModel);
+      };
+    } else if (baseModel.type != BaseModelsTypes.kind) {
+      isOpen = null;
+      onPressed = null;
+    } else {
+      isOpen = treeController.getExpansionState(baseModel);
+      onPressed = () => treeController.toggleExpansion(baseModel);
+    }
+
+    return FolderButton(
+      isOpen: isOpen,
+      onPressed: onPressed,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text("Топология");
+    return AnimatedTreeView(
+        treeController: treeController,
+        nodeBuilder: (context, entry) {
+          return TreeIndentation(
+              child: Row(children: [
+                getLeading(entry.node),
+                Text(entry.node.name ?? "")
+              ]),
+              entry: entry);
+        });
+    ;
   }
 }
 
